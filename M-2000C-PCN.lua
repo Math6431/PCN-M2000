a303b5c6c58add800647e82b4f2cf9453874b2f1
Math6431 <mathieu.clasquin@gmail.com>
2025-06-16 23:15:56 +0200

    Add files via upload

--- M-2000C-PCN.lua
+++ M-2000C-PCN.lua
@@ -1,24 +1,32 @@
-BIOS.protocol.beginModule("M-2000C-PCN", 0x0001)
+BIOS.protocol.beginModule("M-2000C-PCN", 0x0002)
 BIOS.protocol.setExportModuleAircrafts({"M-2000C"})
 
 local lfs = require("lfs")
 
 moduleBeingDefined.exportHooks[#moduleBeingDefined.exportHooks+1] = function()
-    local file = io.open(lfs.writedir() .. "/Logs/pcn_debug.log", "a")
-    if not file then return end
+    for i = 0, 20 do
+        local li = list_indication(i)
+        local file = io.open(lfs.writedir() .. "/Logs/pcn_debug_index_" .. i .. ".log", "a")
+        if not file then return end
 
-    local li = list_indication(9)
-    if not li then
-        file:write("PCN DEBUG: list_indication(9) is nil\n")
+        if li then
+            local hasData = false
+            for k, v in pairs(li) do
+                if not hasData then
+                    file:write(string.format("INDEX %d:
+", i))
+                    hasData = true
+                end
+                file:write(string.format("  %s = %s\n", tostring(k), tostring(v)))
+            end
+            if not hasData then
+                file:write(string.format("INDEX %d: empty table\n", i))
+            end
+        else
+            file:write(string.format("INDEX %d: nil\n", i))
+        end
         file:close()
-        return
     end
-
-    file:write("PCN DEBUG: Dumping list_indication(9)\n")
-    for k, v in pairs(li) do
-        file:write(string.format("PCN DEBUG: %s = %s\n", tostring(k), tostring(v)))
-    end
-    file:close()
 end
 
 BIOS.protocol.endModule()
